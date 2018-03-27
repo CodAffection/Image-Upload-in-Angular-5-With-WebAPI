b4dcc5acb8f9654e90a62eaa0d6feaef3e2c1c1d
DotnetMob <dotnetmob@gmail.com>
2018-03-27 22:33:07 +0530

    WebAPI

---
+++ WebAPI/WebAPI/Controllers/ImageController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ImageController : ApiController
+    {
+
+        [HttpPost]
+        [Route("api/UploadImage")]
+        public HttpResponseMessage UploadImage()
+        {
+            string imageName = null;
+            var httpRequest = HttpContext.Current.Request;
+            //Upload Image
+            var postedFile = httpRequest.Files["Image"];
+            //Create custom filename
+            imageName = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(postedFile.FileName);
+            var filePath = HttpContext.Current.Server.MapPath("~/Image/" + imageName);
+            postedFile.SaveAs(filePath);
+
+            //Save to DB
+            using (DBModel db = new DBModel())
+            {
+                Image image = new Image()
+                {
+                    ImageCaption = httpRequest["ImageCaption"],
+                    ImageName = imageName
+                };
+                db.Images.Add(image);
+                db.SaveChanges();
+            }
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
